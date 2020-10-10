4dc6cf30db397d7464dca989173fd7ee60831e71
Member <feher.janos.zoltan@gmail.com>
2020-10-10 21:45:09 +0200

    Enough for today.

--- Boo-T.py
+++ Boo-T.py
@@ -19,8 +19,7 @@ class Config_Real(ABC):
         self.__CheckBrowsers(self.__Config["Chrome"],
                              self.__Config["FireFox"],
                              self.__Config["Edge"],
-                             self.__Config["Opera"],
-                             self.__Config["Safari"])
+                             self.__Config["Opera"])
 
     @abstractmethod
     def Set_Element(self, key, value):
@@ -39,26 +38,43 @@ class Config_Real(ABC):
             __temp[line.split("=")[0]]=line.split("=")[1].replace("\n","")
         return(__temp)
 
-    def __CheckBrowsers(self, Chrome, FireFox, Edge, Opera, Safari):
+    def __CheckBrowsers(self, Chrome, FireFox, Edge, Opera):
         if Chrome=="":
             self.__Config["Chrome"]=self.__GetLocation("Chrome")
+        if FireFox=="":
+            self.__Config["FireFox"]=self.__GetLocation("FireFox")
+        if Edge=="":
+            self.__Config["Edge"]=self.__GetLocation("Edge")
+        if Opera=="":
+            self.__Config["Opera"]=self.__GetLocation("Opera")
+        print(self.__Config)
+
 
     def __GetLocation(self, browser):
         import winapps
         result=""
         for app in winapps.search_installed(browser):
             if app!="":
-                self.__Regex_Get_Install_Location(app)
+                result=self.__Regex_Get_Install_Location(app)
+
         if result=="":
-            return(result)
+            return("")
         else:
-            pass
+            return(self.__Get_App_Path(result, browser))
+
 
     def __Regex_Get_Install_Location(self, app):
         FindRegex = re.findall(r"install_location=WindowsPath\(\'[a-zA-Z0-9:\/\s]+\'\)", str(app))
         if len(FindRegex) > 0:
             result = FindRegex[0].replace("install_location=WindowsPath('", "").replace("')", "")
         return(result)
+
+    def __Get_App_Path(self, result, browser):
+        for root, dirs, files in os.walk(result):
+            for file in files:
+                if file.upper()==browser.upper()+".EXE":
+                    return(str(root + "/" + file).replace("\\", "/"))
+        return("")
 
 class Config(Config_Real):
     """This is the class that can be accessed directly for the
